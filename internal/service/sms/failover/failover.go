51fe5b32692931aea598ef8d160215d022518b1e
huangyul <1103221645@qq.com>
2024-12-05 16:32:36 +0800

    feat: service governance

--- internal/service/sms/failover/failover.go
+++ internal/service/sms/failover/failover.go
@@ -19,13 +19,13 @@ func (s *SMSService) Send(ctx context.Context, tplId string, args []string, numb
 	for i := idx; i < length+idx; i++ {
 		svc := s.svcs[i%length]
 		err := svc.Send(ctx, tplId, args, number...)
-		switch err {
-		case nil:
+		switch {
+		case err == nil:
 			if i != idx {
 				atomic.StoreUint32(&s.idx, i)
 			}
 			return nil
-		case context.Canceled, context.DeadlineExceeded:
+		case errors.Is(err, context.Canceled), errors.Is(err, context.DeadlineExceeded):
 			return err
 		}
 	}
